e684f26c8391229183e53ea1c58605fbb9aa3566
Johanexx <kevinjoha342@gmail.com>
2025-09-09 17:50:16 -0500

    feat(frontend): 9 septiembre

--- 01-bases-react/src/Components/BasicTypes.tsx
+++ 01-bases-react/src/Components/BasicTypes.tsx
@@ -6,35 +6,22 @@ export const BasicTypes = () => {
 
 const [age, setAge] = useState(20);
 const [isActive, setIsActive] = useState(true);
+
+const names = [
+
+"Armando Casas1",
+"Armando Casas2",
+"Dolores Fuertes",
+"Armando Casas4",
+"Armando Casas5",
+
+
+];
   return (
     <>
-   <h3>Basic Types</h3>
-      {isActive?
-      <ul>
-        <li>Nombre {name}</li>
-        <li>Edad {age}</li>
-      </ul>:
-      <p>Usuario inactivo</p>
-      }
+   
+     {names[0]}
 
-
-
-<h2>Variable de estado 1</h2>
-<button onClick={()=>{
-        setIsActive(!isActive);
-      }}>
-        {isActive?"Desactivar": "Activar"} Usuario
-        </button>
-
-<  h2>Variable de estado 2</h2>
-<button onClick={
-  () => {
-    setAge(21);
-    console.log(`l valor de age es: ${age}`)
-  }
-  
-
-}>Modificar Edad</button>
     </>
 
   )
--- 02-bases-react/src/components/BasicTypes.tsx
+++ 02-bases-react/src/components/BasicTypes.tsx
@@ -1,21 +1,67 @@
 import React, { useState } from 'react'
 
 export const BasicTypes = () => {
-    const name: string = 'Temu';
-    const age: number = 30;
+  const name: string = 'Temu';
+  const age: number = 30;
 
-    const [isActive,setIsActive]=useState(true)
+  const [isActive, setIsActive] = useState(true)
+  const names = [
+
+    "Armando Casas1",
+    "Armando Casas2",
+    "Dolores Fuertes",
+    "Armando Casas4",
+    "Armando Casas5",];
+
+  const namesMascotas = [
+
+    "Armando el lion",
+    "pepe el toro",
+    "Dolores sociales",
+    "JUano choclo",
+    "cULEBRA",];
+    const user ={
+      name: "JUanA",
+      email: "j.ne@gmail.com",
+      age: "20",
+      role: "administrador",
+      MaritalStatus: "casado",
+      isActive: true
+
+    }
   return (
     <>
-    <div>Pagina para come queso XD</div>
-    <h2>Constantes</h2>
-    <ul>
+      <div>Pagina para come queso XD</div>
+      <h2>Constantes</h2>
+      <ul>
         <li><b>name </b><span>{name}</span></li>
         <li><b>age </b><span>{age}</span></li>
-        <li><b>isActive </b><span>{isActive?"usuario activo:":"usuario inactivo"}</span></li>
-    </ul>
- 
- <button onClick={()=> setIsActive(!isActive)}>click Temu{isActive?"usuario activo:":"usuario inactivo"}</button>
-</>
-)
+        <li><b>isActive </b><span>{isActive ? "usuario activo:" : "usuario inactivo"}</span></li>
+      </ul>
+
+      <button onClick={() => setIsActive(!isActive)}>click Temu{isActive ? "usuario activo:" : "usuario inactivo"}</button>
+
+
+      <ul>
+        {names.map(
+          (name, index) => <li key={index}>
+            {name}
+          </li>
+        )}
+      </ul>
+
+      <div>
+        {namesMascotas.map(
+          (name, index) => <div key={index}>
+            {name}
+          </div>
+        )}
+{namesMascotas.join( ", ")}
+
+<h2>Objetos Literales</h2>
+{user.name}
+      </div>
+
+    </>
+  )
 }
